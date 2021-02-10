a416f2f950300f1a3c2dd34d202638344909470f
marcin50 <mlenart50@gmail.com>
2021-02-09 19:06:44 -0500

    Update script_paceplot.js

--- js/script_paceplot.js
+++ js/script_paceplot.js
@@ -9,7 +9,7 @@ const dateBox = document.getElementById('date');
 
 //chart inits
 canvas.width = 700;
-canvas.height = 500;
+canvas.height = 440;
 let xGrid = 10;
 let yGrid = 10;
 let cellSize = 10;
@@ -25,21 +25,21 @@ function onCreate() {
     //check if there is anything in local storage and load it
     if (localStorage.length > 0) {
         let jsonEntries = JSON.parse(localStorage['entries-local']);
+
         //transfer local storage to entries array
         for (let i in jsonEntries)
-            entries.push(new Entry(jsonEntries[i].pace, jsonEntries[i].date));
+            entries.push(new Entry(jsonEntries[i].pace, (jsonEntries[i].date), false));
 
-        console.log(entries);
+
         createList();
+
     }
-    drawGrids();
+    // drawGrids();
     drawAxis();
     drawChart();
 }
 
-
-
-// add new entry based on the form inputs
+// add new entry from the pace and date inputs when button is pressed
 function addEntry() {
     //get the pace and date from input boxes
     let paceEntry = parsePace();
@@ -50,22 +50,24 @@ function addEntry() {
         return;
     }
 
-
-    let entry = new Entry(paceEntry, dateEntry);
+    let entry = new Entry(paceEntry, dateEntry, true);
     entries.push(entry);
-    //delete later************************************************
-    entries.push(new Entry('5:57', '2020-02-06'));
-    entries.push(new Entry('5:22', '2021-01-01'));
-    entries.push(new Entry('5:23', '2020-06-02'));
-    console.log("min:"  + entry.getMin());
-    console.log("sec:" + entry.getSec());
+
+
+    //delete later***************************************************************************
+    entries.push(new Entry('5:57', '2020-02-09', true));
+    entries.push(new Entry('3:22', '2021-01-01', true));
+    entries.push(new Entry('5:23', '2020-06-02', true));
+
     sortEntries();
 
     //save to local storage
     localStorage['entries-local'] = JSON.stringify(entries);
 
     createList();
-
+    ctx.clearRect(0, 0, canvas.width, canvas.height); //clear existing canvas
+    drawAxis();
+    drawChart();
 }
 
 //check for correct pace input format and fix.
@@ -97,7 +99,6 @@ function parseDate() {
         date = today;
     }
 
-
     return "" + date;
 }
 
@@ -106,11 +107,11 @@ function createList() {
     //clear all items from list
     while (dataList.firstChild) dataList.removeChild(dataList.firstChild);
 
-    //add all list items from array
+    //add all list items from entries array
     for (let i in entries) {
         li = document.createElement('li');
         // Add text node with input values
-        li.appendChild(document.createTextNode(`${entries[i].pace}: ${entries[i].date}`));
+        li.appendChild(document.createTextNode(`${entries[i].pace}: ${entries[i].date.toDateString()}`));
         dataList.appendChild(li);
         // dataList.prependChild(li); need somethign liek this . this function doesnt exist
     }
@@ -124,8 +125,13 @@ function sortEntries() {
 
 
 function resetAll() {
+    //reset date
     entries = [];
     localStorage.clear();
+    
+    //clear most of the chart
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawAxis();
 
     //remove all items from list
     while (dataList.firstChild) dataList.removeChild(dataList.firstChild);
@@ -134,10 +140,13 @@ function resetAll() {
 
 //entry class for paces and dates
 class Entry {
-    constructor(pace, date) {
-        this.pace = pace
-        this.date = new Date(date)
-        //this.date = new Date(date.split('-'));
+    constructor(pace, date, format) {
+        this.pace = pace;
+        //fixes the date timezone depending on the input style
+        if (format) {
+            this.date = new Date(date.replace(/-/g, '/'));
+        } else this.date = new Date(date);
+
     }
 
     // Get Birth Year
@@ -146,11 +155,12 @@ class Entry {
     }
     // Get Birth Year
     getMin() {
-        return this.pace.split(':')[0];
+
+        return parseInt(this.pace.split(':')[0]);
     }
     // Get Birth Year
     getSec() {
-        return this.pace.split(':')[1];
+        return parseInt(this.pace.split(':')[1]);
     }
     // Get Birth Year
     getYear() {
@@ -182,7 +192,10 @@ function getDateAxis() {
 }
 
 function drawAxis() {
+
     //initialize cursor to bottom left of chart
+    //bottom left of chart is: (blocks(5), blocks(40)) in terms of x,y plane
+
     var xPlot = 5;
     var yPlot = 40;
 
@@ -191,16 +204,19 @@ function drawAxis() {
     //draw axis lines
     ctx.beginPath();
     ctx.strokeStyle = "black";
-    ctx.moveTo(blocks(5), blocks(5));
+    ctx.moveTo(blocks(5), blocks(2));
     ctx.lineTo(blocks(5), blocks(40));
     ctx.lineTo(blocks(80), blocks(40));
 
-    //set cursor to bottom left of chart
-    ctx.moveTo(blocks(5), blocks(40));
 
-    //draw Pace axis labels
+    //Pace axis
     for (let i = 1; i <= 10; i++) {
-        ctx.strokeText(`${time}:00`, blocks(2), blocks(yPlot));
+        //draw ticks
+        ctx.moveTo(blocks(4), blocks(yPlot));
+        ctx.lineTo(blocks(6), blocks(yPlot));
+
+        //draw pace label
+        ctx.strokeText(`${time}:00`, blocks(1.75), blocks(yPlot));
         yPlot -= 4;
         time += 1;
     }
@@ -214,14 +230,19 @@ function drawAxis() {
     //date range interval using 12 labels
     let xInterval = (xMax - xMin) / 12;
 
+    //reduce font for date labels so they fit
+    ctx.font = "9px sans-serif";
 
-    //draw date axis labels
+    //date axis
     for (let i = 1; i <= 13; i++) {
+        //draw ticks
+        ctx.moveTo(blocks(xPlot), blocks(41));
+        ctx.lineTo(blocks(xPlot), blocks(39));
 
+        //draw date label
         let shortDate = `${xMin.getFullYear()}/${xMin.getMonth() + 1}/${xMin.getDate()}`;
+        ctx.strokeText(shortDate, blocks(xPlot-1), blocks(42));
 
-        ctx.strokeText(shortDate, blocks(xPlot), blocks(42));
-        ctx.arc(blocks(xPlot), blocks(42), 2, 0, Math.PI * 2, true);
         //date label spacing
         xPlot += 5;
 
@@ -229,49 +250,49 @@ function drawAxis() {
         xMin.setTime(xMin.getTime() + xInterval);
 
     }
+        //reduce font for date labels so they fit
+        ctx.font = "12px sans-serif";
     ctx.stroke();
+
 }
 
+//draws the chart data points and line connection for each entry that fits in the range
 function drawChart() {
-
     ctx.beginPath();
-    ctx.strokeStyle = "black";
+    ctx.strokeStyle = "blue";
 
-    //set cursor to bottom left corner of chart initially
-    ctx.moveTo(blocks(5), blocks(40));
 
     var xMax = new Date();
     var xMin = new Date();
     xMin.setMonth(xMin.getMonth() - 12);
-
+var firstElement=true;
     //plot each entry if it is in the range
     for (let entry of entries) {
-        //make and if statement to check if its in the range of xmin and xmax or ignore it********************
-        // console.log(entry.pace + " " + entry.date);
-        let shortDate = `${entry.date.getFullYear()}/${entry.date.getMonth() + 1}/${entry.date.getDate()}`;
+        if (entry.date > xMin && entry.date < xMax) {
+            //make and if statement to check if its in the range of xmin and xmax or ignore it********************
+            // console.log(entry.pace + " " + entry.date);
+            let shortDate = `${entry.date.getFullYear()}/${entry.date.getMonth() + 1}/${entry.date.getDate()}`;
 
-        //calculate location of pace coordinate
-        let m = 5;
-        let s = 46;
-        let paceLocation = (m * 4) + (s / 15);
+            //calculate location of pace coordinate
+            let m = entry.getMin();
+            let s = entry.getSec();
+            let paceLocation = (m * 4) + (s / 15);
 
-        //calculate location of date coordinate
-        //plan - we have 60 blocks of usable space, can get date in ms
-        //range=xmax-min
-        //value=date-xmin
-        //value=value*60blocks
-        let xRange = xMax.getTime() - xMin.getTime();
-        console.log("Range " + xRange);
-        let wee = (entry.date.getTime() - xMin.getTime());
-        console.log("wee" + wee);
-        console.log("date: " + entry.date + " xmin: " + xMin);
-        dateLocation = ((wee / xRange) * 60) + 5;//add 5 for axis offset
-
-        console.log(dateLocation);
-
-        ctx.strokeText(m + ":" + s + " " + shortDate, blocks(dateLocation), blocks(40 - paceLocation - 1));
-        ctx.lineTo(blocks(dateLocation), blocks(40 - paceLocation));
-        ctx.arc(blocks(dateLocation), blocks(40 - paceLocation), 2, 0, Math.PI * 2, true);
+            //calculate location of date coordinate
+            //plan - we have 60 blocks of usable space, can get date in ms
+            //range=xmax-min
+            //value=date-xmin
+            //value=value*60blocks
+            let xRange = xMax.getTime() - xMin.getTime();
+            let dateLocation = (entry.date.getTime() - xMin.getTime());
+            dateLocation = ((dateLocation / xRange) * 60) + 5;//add 5 for axis offset
+            ctx.strokeText(m + ":" + s + " " + shortDate, blocks(dateLocation), blocks(40 - paceLocation - 1));
+            //used so that the line starts at the first element location
+            if (firstElement){ctx.moveTo(blocks(dateLocation), blocks(40 - paceLocation));}
+            ctx.lineTo(blocks(dateLocation), blocks(40 - paceLocation));
+            ctx.arc(blocks(dateLocation), blocks(40 - paceLocation), 2, 0, Math.PI * 2, true);
+            firstElement=false;
+        }
     }
     ctx.stroke();
 }
